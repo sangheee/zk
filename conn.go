9b407a2e088b83eb04638f153b0c6d92e8492202
sangheee <afk.sh94@gmail.com>
2020-11-03 11:55:17 +0900

    Update conn.go

--- conn.go
+++ conn.go
@@ -1068,7 +1068,7 @@ func (c *Conn) CreateTTL(path string, data []byte, flags int32, acl []ACL, ttl t
 	}
 
 	res := &createResponse{}
-	_, err := c.request(opCreateTTL, &CreateTTLRequest{path, data, acl, flags, ttl.Milliseconds()}, res, nil)
+	_, err := c.request(opCreateTTL, &CreateTTLRequest{path, data, acl, flags, int64(ttl / time.Millisecond)}, res, nil)
 	return res.Path, err
 }
 
